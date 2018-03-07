aa7e331e97a37a3478b0fcd76ed41c8950150fad
Stephanie Youstra <steph.you07@gmail.com>
2018-03-06 22:58:58 -0500

    feat: Override onClick (Lesson 4.23)

--- S03.02-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S03.02-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -44,20 +44,28 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
         mClickHandler = clickHandler;
     }
 
-    // TODO (5) Implement OnClickListener in the ForecastAdapterViewHolder class
+    // COMPLETED (5) Implement OnClickListener in the ForecastAdapterViewHolder class
     /**
      * Cache of the children views for a forecast list item.
      */
-    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public final TextView mWeatherTextView;
 
         public ForecastAdapterViewHolder(View view) {
             super(view);
             mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
-            // TODO (7) Call setOnClickListener on the view passed into the constructor (use 'this' as the OnClickListener)
+            // COMPLETED (7) Call setOnClickListener on the view passed into the constructor (use 'this' as the OnClickListener)
+            view.setOnClickListener(this);
         }
 
-        // TODO (6) Override onClick, passing the clicked day's data to mClickHandler via its onClick method
+        // COMPLETED (6) Override onClick, passing the clicked day's data to mClickHandler via its onClick method
+
+        @Override
+        public void onClick(View v) {
+            int clickedPosition = getAdapterPosition();             //LINE FROM SOLUTION
+            String clickedWeather = mWeatherData[clickedPosition]   //LINE FROM SOLUTION
+            mClickHandler.onClick(clickedWeather);
+        }
     }
 
     /**
