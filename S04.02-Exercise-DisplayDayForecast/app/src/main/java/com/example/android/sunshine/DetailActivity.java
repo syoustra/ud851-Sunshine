fb81733363d1e54fcff1ba66a10853ae7d30d8cc
Stephanie Youstra <steph.you07@gmail.com>
2018-03-09 16:06:50 -0500

    feat: Pass weather data to DetailActivity (Lesson 5.24)

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,11 +1,14 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private TextView mDisplayWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +16,13 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         // TODO (2) Display the weather forecast that was passed from MainActivity
+        mDisplayWeather = (TextView) findViewById(R.id.tv_weather_data);
+
+        Intent originatingIntent = getIntent();
+        if (originatingIntent.hasExtra(Intent.EXTRA_TEXT)) {
+            String weatherToDisplay = originatingIntent.getStringExtra(Intent.EXTRA_TEXT);
+            mDisplayWeather.setText(weatherToDisplay);
+
+        }
     }
 }
