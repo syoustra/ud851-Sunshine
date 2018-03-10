1d0180d3098948325e80a2ce61bbedbdf0c51983
Stephanie Youstra <steph.you07@gmail.com>
2018-03-09 21:52:45 -0500

    feat: Display Share menu button on forecast detail screen (Lesson 5.25)

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -3,6 +3,8 @@ package com.example.android.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -31,4 +33,12 @@ public class DetailActivity extends AppCompatActivity {
 
     // TODO (3) Create a menu with an item with id of action_share
     // TODO (4) Display the menu and implement the forecast sharing functionality
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.share, menu);
+
+        return true;
+    }
 }
