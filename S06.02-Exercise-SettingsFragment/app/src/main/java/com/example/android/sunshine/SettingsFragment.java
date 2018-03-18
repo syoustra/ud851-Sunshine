77fe2e99d98e925a6a5893a2baf5e92a2fc89a7d
Stephanie Youstra <steph.you07@gmail.com>
2018-03-18 13:36:48 -0400

    feat: Set preferenceSummary (Lesson 7.34)

--- S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
+++ S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.example.android.sunshine;
 
 import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
 /**
@@ -11,5 +13,20 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_general);
+    }
+
+    private void setPreferenceSummary(Preference preference, Object value) {
+        String stringValue = value.toString();                                   //THESE TWO FROM SOLUTION
+        String key = preference.getKey();
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {                                                                //ALSO FROM SOLUTION
+            preference.setSummary(stringValue);
+        }
     }
 }
