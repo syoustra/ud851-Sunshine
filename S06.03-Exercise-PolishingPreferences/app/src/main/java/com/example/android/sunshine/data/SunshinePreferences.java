858e2c159ab99a47672b72f395d50e16e9d980b1
Stephanie Youstra <steph.you07@gmail.com>
2018-03-18 19:47:12 -0400

    docs: Clear TODOs (Lesson 7.35)

--- S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -91,7 +91,7 @@ public class SunshinePreferences {
      * "94043,USA" if SharedPreferences have not been implemented yet.
      */
     public static String getPreferredWeatherLocation(Context context) {
-        // TODO (1) Return the user's preferred location
+        // COMPLETED (1) Return the user's preferred location
         /** This will be implemented in a future lesson **/                         //THIS METHOD FROM SOLUTION
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         String keyForLocation = context.getString(R.string.pref_location_key);
@@ -107,7 +107,7 @@ public class SunshinePreferences {
      * @return true If metric display should be used
      */
     public static boolean isMetric(Context context) {
-        // TODO (2) Return true if the user's preference for units is metric, false otherwise
+        // COMPLETED (2) Return true if the user's preference for units is metric, false otherwise
         /** This will be implemented in a future lesson **/                         //THIS METHOD FROM SOLUTION
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         String keyForUnits = context.getString(R.string.pref_units_key);
