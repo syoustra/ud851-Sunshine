713d14b5cc48861066f29043a7f57ca0040ad8e6
Stephanie Youstra <steph.you07@gmail.com>
2018-03-23 02:22:40 -0400

    feat: Add functionality to delete all data from table (Lesson 10.39)

--- S09.03-Exercise-ContentProviderDelete/app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
+++ S09.03-Exercise-ContentProviderDelete/app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
@@ -304,11 +304,21 @@ public class WeatherProvider extends ContentProvider {
      */
     @Override
     public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
-        throw new RuntimeException("Student, you need to implement the delete method!");
+        SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        int deletedRowCount;
 
 //          TODO (2) Only implement the functionality, given the proper URI, to delete ALL rows in the weather table
+        switch (sUriMatcher.match(uri)) {
+            case CODE_WEATHER:
+                deletedRowCount = db.delete(WeatherContract.WeatherEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
 
 //      TODO (3) Return the number of rows deleted
+        return deletedRowCount;
     }
 
     /**
