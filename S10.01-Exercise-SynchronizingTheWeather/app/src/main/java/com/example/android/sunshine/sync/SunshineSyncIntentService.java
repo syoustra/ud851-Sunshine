ae79dabcf4a94d5249510cd279829c6cac64519b
Stephanie Youstra <steph.you07@gmail.com>
2018-03-25 01:02:22 -0400

    feat: Create SunshineSyncIntentService and override onHandleIntent (Lesson 11.30)

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
@@ -1,4 +1,23 @@
-// TODO (5) Create a new class called SunshineSyncIntentService that extends IntentService
+package com.example.android.sunshine.sync;
 
-//  TODO (6) Create a constructor that calls super and passes the name of this class
-//  TODO (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+// TODO (5) Create a new class called SunshineSyncIntentService that extends IntentService
+public class SunshineSyncIntentService extends IntentService {
+
+    //  TODO (6) Create a constructor that calls super and passes the name of this class
+
+    public SunshineSyncIntentService(String SunshineSyncIntentService) {
+        super("SunshineSyncIntentService");
+    }
+
+    //  TODO (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        SunshineSyncTask.syncWeather(this);
+
+    }
+}
