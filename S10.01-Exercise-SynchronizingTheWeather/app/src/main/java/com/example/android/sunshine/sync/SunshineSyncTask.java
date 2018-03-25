d698ccdc25a0292349a0315aaa9ff122e9f805e5
Stephanie Youstra <steph.you07@gmail.com>
2018-03-25 00:54:17 -0400

    feat: Create SunshineSyncTask, to delete old weather data and replace with new (Lesson 11.30)

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,34 @@
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import java.net.URL;
+
 //  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
-//      TODO (3) Within syncWeather, fetch new weather data
-//      TODO (4) If we have valid results, delete the old data and insert the new
+public class SunshineSyncTask {
+    //  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
+    synchronized public static void syncWeather (Context context) {
+
+        //      TODO (3) Within syncWeather, fetch new weather data
+        try {
+            URL weatherRequestUrl = NetworkUtils.getUrl(context);
+            String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
+            ContentValues[] weatherValues = OpenWeatherJsonUtils.getWeatherContentValuesFromJson(context, jsonWeatherResponse);
+
+            //      TODO (4) If we have valid results, delete the old data and insert the new
+            if (weatherValues != null && weatherValues.length != 0) {
+                ContentResolver sunshineContentResolver = context.getContentResolver();
+                sunshineContentResolver.delete(WeatherContract.WeatherEntry.CONTENT_URI, null, null);
+                sunshineContentResolver.bulkInsert(WeatherContract.WeatherEntry, weatherValues);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
