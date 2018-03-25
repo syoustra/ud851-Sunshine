a5227269832952c67d980d6e96b75b269e0f9ac3
Stephanie Youstra <steph.you07@gmail.com>
2018-03-25 01:22:00 -0400

    feat: Remove fake data and call startImmediateSync method (Lesson 11.30)

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -34,7 +34,9 @@ import android.widget.ProgressBar;
 
 import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.sync.SunshineSyncUtils;
 import com.example.android.sunshine.utilities.FakeDataUtils;
+import com.example.android.sunshine.utilities.SunshineWeatherUtils;
 
 public class MainActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor>,
@@ -87,7 +89,6 @@ public class MainActivity extends AppCompatActivity implements
         getSupportActionBar().setElevation(0f);
 
         // TODO (12) Remove the fake data creation since we can now sync with live data
-        FakeDataUtils.insertFakeData(this);
 
         /*
          * Using findViewById, we get a reference to our RecyclerView from xml. This allows us to
@@ -156,6 +157,7 @@ public class MainActivity extends AppCompatActivity implements
         getSupportLoaderManager().initLoader(ID_FORECAST_LOADER, null, this);
 
         //  TODO (13) Call SunshineSyncUtils's startImmediateSync method
+        SunshineSyncUtils.startImmediateSync(this);
 
     }
 
